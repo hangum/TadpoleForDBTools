16ab80a64f422872ffd066ae45ce62e49ea715bf
cho hyun jong <hangum@gmail.com>
2013-01-31 00:26:41 +0900

    add comment

--- com.hangum.db.commons.libs/src/com/hangum/tadpold/commons/libs/core/browser/TadpoleBrowserFunction.java
+++ com.hangum.db.commons.libs/src/com/hangum/tadpold/commons/libs/core/browser/TadpoleBrowserFunction.java
@@ -1,6 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Cho Hyun Jong.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Cho Hyun Jong - initial API and implementation
+ ******************************************************************************/
 package com.hangum.tadpold.commons.libs.core.browser;
 
+/**
+ * OrionHub를 사용한 에디터에서 공통으로 사용하기 위한 유틸
+ * 
+ * @author hangum
+ *
+ */
 public interface TadpoleBrowserFunction {
+
 	/** 오리온허브 에디터에서 사용하는 딜리미터 */
 	public static final String CARET_QUERY_DELIMIT = "-tadpole-del-";
 }
