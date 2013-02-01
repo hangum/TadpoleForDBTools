43f85873c0f1b0785e0431fa5f5da9cb55b0a3f7
cho hyun jong <hangum@gmail.com>
2013-02-02 00:45:07 +0900

    - Mac에서 icns 추가 - standalone 모드에서 browser 동기화 되도록 추가

--- com.hangum.db.rap.commons/src/com/hangum/tadpole/start/TadpoleOpenBrowser.java
+++ com.hangum.db.rap.commons/src/com/hangum/tadpole/start/TadpoleOpenBrowser.java
@@ -33,7 +33,7 @@ public class TadpoleOpenBrowser  implements Runnable {
 		
 		try {
 			// 시스템 속도가 느려서 워크벤치가 정상적으로 동작하지 못했을 경우에 대비하여 기다립니다.
-			try { Thread.sleep(1000); } catch(Exception e) {};
+			try { Thread.sleep(3000); } catch(Exception e) {};
 			
 			// window
 			if (s1.indexOf("windows") >= 0) {
