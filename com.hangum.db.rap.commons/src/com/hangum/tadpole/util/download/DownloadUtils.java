c20dfabc6c0229ce693af2a37ef51415370943f2
cho hyun jong <hangum@gmail.com>
2013-02-21 21:44:35 +0900

    RAP2.0에 맞도록 download util 수정

--- com.hangum.db.rap.commons/src/com/hangum/tadpole/util/download/DownloadUtils.java
+++ com.hangum.db.rap.commons/src/com/hangum/tadpole/util/download/DownloadUtils.java
@@ -12,7 +12,7 @@ package com.hangum.tadpole.util.download;
 
 
 import org.eclipse.rap.rwt.RWT;
-import org.eclipse.rap.rwt.service.ServiceHandler;
+import org.eclipse.rap.rwt.service.ServiceManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.widgets.Composite;
@@ -27,14 +27,10 @@ public class DownloadUtils {
 
 	/** download url */
 	private static String createDownloadUrl(String id) {
-		StringBuilder url = new StringBuilder();
-		url.append(RWT.getRequest().getContextPath());
-		url.append(RWT.getRequest().getServletPath());
-		url.append("?");
-		url.append("custom_service_handler");//ServiceHandler.REQUEST_PARAM);
-		url.append("=" + id);
-		String encodedURL = RWT.getResponse().encodeURL(url.toString());
-		return encodedURL;
+		ServiceManager manager = RWT.getServiceManager();
+		String url = manager.getServiceHandlerUrl( id );
+		
+		return url;
 	}
 
 	/**
