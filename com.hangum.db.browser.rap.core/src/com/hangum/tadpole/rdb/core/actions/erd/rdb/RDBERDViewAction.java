67b02fa29d68374418e9748e2aac58106ce39cbf
cho hyun jong <hangum@gmail.com>
2013-03-02 23:48:23 +0900

    툴바버튼에서 몽고디비 erd 보기 눌렀을 경우 에러 수정

--- com.hangum.db.browser.rap.core/src/com/hangum/tadpole/rdb/core/actions/erd/rdb/RDBERDViewAction.java
+++ com.hangum.db.browser.rap.core/src/com/hangum/tadpole/rdb/core/actions/erd/rdb/RDBERDViewAction.java
@@ -22,9 +22,12 @@ import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 
+import com.hangum.tadpole.commons.sql.define.DBDefine;
 import com.hangum.tadpole.dao.system.UserDBDAO;
 import com.hangum.tadpole.dao.system.UserDBResourceDAO;
 import com.hangum.tadpole.exception.dialog.ExceptionDetailsErrorDialog;
+import com.hangum.tadpole.mongodb.erd.core.editor.TadpoleMongoDBERDEditor;
+import com.hangum.tadpole.mongodb.erd.core.editor.TadpoleMongoDBEditorInput;
 import com.hangum.tadpole.rdb.core.Activator;
 import com.hangum.tadpole.rdb.core.Messages;
 import com.hangum.tadpole.rdb.erd.core.editor.TadpoleRDBEditor;
@@ -50,8 +53,13 @@ public class RDBERDViewAction implements IViewActionDelegate {
 	public void run(UserDBDAO userDB) {
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();		
 		try {
-			TadpoleRDBEditorInput input = new TadpoleRDBEditorInput(userDB.getDisplay_name() + "(" + userDB.getDb() + ")", userDB, false);
-			page.openEditor(input, TadpoleRDBEditor.ID, false);
+			if(DBDefine.getDBDefine(userDB.getTypes()) == DBDefine.MONGODB_DEFAULT) {
+				TadpoleMongoDBEditorInput input = new TadpoleMongoDBEditorInput(userDB.getDisplay_name() + "(" + userDB.getDb() + ")", userDB, false);
+				page.openEditor(input, TadpoleMongoDBERDEditor.ID, false);
+			} else {
+				TadpoleRDBEditorInput input = new TadpoleRDBEditorInput(userDB.getDisplay_name() + "(" + userDB.getDb() + ")", userDB, false);
+				page.openEditor(input, TadpoleRDBEditor.ID, false);				
+			}
 			
 		} catch (PartInitException e) {
 			logger.error("erd editor opend", e);
@@ -65,8 +73,14 @@ public class RDBERDViewAction implements IViewActionDelegate {
 		UserDBDAO userDB = userDBErd.getParent();
 		IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();		
 		try {
-			TadpoleRDBEditorInput input = new TadpoleRDBEditorInput(userDB.getDisplay_name() + "(" + userDB.getDb() + ")", userDBErd);
-			page.openEditor(input, TadpoleRDBEditor.ID, false);
+			
+			if(DBDefine.getDBDefine(userDB.getTypes()) == DBDefine.MONGODB_DEFAULT) {
+				TadpoleMongoDBEditorInput input = new TadpoleMongoDBEditorInput(userDB.getDisplay_name() + "(" + userDB.getDb() + ")", userDB, false);
+				page.openEditor(input, TadpoleMongoDBERDEditor.ID, false);
+			} else {
+				TadpoleRDBEditorInput input = new TadpoleRDBEditorInput(userDB.getDisplay_name() + "(" + userDB.getDb() + ")", userDBErd);
+				page.openEditor(input, TadpoleRDBEditor.ID, false);
+			}
 			
 		} catch (PartInitException e) {
 			logger.error("erd editor opend", e);
