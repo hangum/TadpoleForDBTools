23aac73a612e3e5a5da7cf5003be29ecead9035e
hangum <hangum@gmail.com>
2013-03-09 09:18:58 +0900

    code refactoring

--- com.hangum.tadpole.mongodb.core/src/com/hangum/tadpole/mongodb/core/composite/result/MongodbResultComposite.java
+++ com.hangum.tadpole.mongodb.core/src/com/hangum/tadpole/mongodb/core/composite/result/MongodbResultComposite.java
@@ -661,7 +661,6 @@ public class MongodbResultComposite extends Composite {
 	 */
 	private void find(BasicDBObject basicFields, DBObject basicWhere, BasicDBObject basicSort, int cntSkip, int cntLimit) throws Exception {
 		if( (cntLimit - cntSkip) >= defaultMaxCount) {
-			
 //			"검색 수가 " + defaultMaxCount + "를 넘을수 없습니다. Prefernece에서 값을 조절하십시오."
 //			Search can not exceed the number 5. Set in Perference.
 			throw new Exception(String.format(Messages.MongoDBTableEditor_0, ""+defaultMaxCount));  //$NON-NLS-2$ //$NON-NLS-1$
