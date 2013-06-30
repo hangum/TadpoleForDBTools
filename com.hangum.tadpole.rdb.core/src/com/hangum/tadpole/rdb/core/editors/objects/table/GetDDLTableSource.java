f9c077dfd18fd0b311ee890380c7a66683fbf528
jeongjaehong <jeongjaehong@gmail.com>
2013-06-30 17:32:41 +0900

    Oracle Table DDL 스크립팅 추가.

--- com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/editors/objects/table/GetDDLTableSource.java
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/editors/objects/table/GetDDLTableSource.java
@@ -10,8 +10,13 @@
  ******************************************************************************/
 package com.hangum.tadpole.rdb.core.editors.objects.table;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
 
 import com.hangum.tadpold.commons.libs.core.define.PublicTadpoleDefine;
 import com.hangum.tadpold.commons.libs.core.define.PublicTadpoleDefine.DB_ACTION;
@@ -28,6 +33,7 @@ import com.ibatis.sqlmap.client.SqlMapClient;
  */
 public class GetDDLTableSource {
 
+	private static final Logger logger = Logger.getLogger(GetDDLTableSource.class);
 	/**
 	 * DDL Source view
 	 * 
@@ -45,6 +51,101 @@ public class GetDDLTableSource {
 			
 			if(PublicTadpoleDefine.DB_ACTION.TABLES == actionType) {
 				return ""+client.queryForObject("getTableScript", objectName);
+			} else if(PublicTadpoleDefine.DB_ACTION.VIEWS == actionType) {
+				return ""+client.queryForObject("getViewScript", objectName);
+			}
+			
+			throw new Exception("Not support Database");
+		} else if(DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.ORACLE_DEFAULT) {
+			SqlMapClient client = TadpoleSQLManager.getInstance(userDB);
+			
+			if(PublicTadpoleDefine.DB_ACTION.TABLES == actionType) {
+				List<HashMap> srcList = client.queryForList("getTableScript", objectName);
+				
+				
+				StringBuilder result = new StringBuilder("");
+				result.append("/* DROP TABLE " + objectName + " CASCADE CONSTRAINT */ \n\n");
+				result.append("CREATE TABLE " + objectName + "( \n");
+				for (int i=0; i<srcList.size(); i++){
+					HashMap<String, Object> source =  srcList.get(i);
+					
+					
+					result.append("\t");
+					if(i>0)result.append(",");
+					result.append(source.get("COLUMN_NAME")).append(" ");
+					result.append(source.get("DATA_TYPE"));
+					
+					if (source.get("DATA_PRECISION") != null && ((BigDecimal)source.get("DATA_PRECISION")).intValue() > 0 ){
+						result.append("("+source.get("DATA_PRECISION"));
+						if (source.get("DATA_SCALE") != null && ((BigDecimal)source.get("DATA_SCALE")).intValue() > 0 ){
+							result.append(","+source.get("DATA_SCALE"));
+						}
+								
+						result.append(")");
+					}else if (!StringUtils.contains((String)source.get("DATA_TYPE"), "DATE") && !StringUtils.contains((String)source.get("DATA_TYPE"), "NUMBER")  && ((BigDecimal)source.get("DATA_LENGTH")).intValue() > 0  ){
+						result.append("("+source.get("DATA_LENGTH")+")");
+					}else{
+						result.append(" ");
+					}
+					
+					if (source.get("DATA_DEFAULT") != null ){
+					
+						if (StringUtils.contains((String)source.get("DATA_TYPE"), "CHAR")  ){
+							result.append(" DEFAULT '"+source.get("DATA_DEFAULT")+"'");
+						}else{
+							result.append(" DEFAULT "+source.get("DATA_DEFAULT") );
+						}
+					}
+						
+					if("N".equals(source.get("NULLABLE"))){
+						result.append(" NOT NULL ");
+					}
+					
+					result.append("\n");
+					
+				}
+
+				// primary key 
+				List<HashMap> srcPkList = client.queryForList("getTableScript.pk", objectName);				
+				for (int i=0; i<srcPkList.size(); i++){
+					HashMap<String, Object> source =  srcPkList.get(i);
+					if(i==0){
+						result.append("\t,CONSTRAINT ").append(source.get("CONSTRAINT_NAME")).append(" PRIMARY KEY ( ").append(source.get("COLUMN_NAME"));
+					}else{
+						result.append(", "+source.get("COLUMN_NAME"));
+					}
+					
+					if(i == srcPkList.size()-1){
+						result.append(") \n");
+					}
+				}
+						
+				result.append("); \n\n");
+				
+				// table, column comments
+				List<String> srcCommentList = client.queryForList("getTableScript.comments", objectName);				
+				for (int i=0; i<srcCommentList.size(); i++){
+					result.append( srcCommentList.get(i)+"\n");
+				}
+				
+				// foreign key
+				
+				// column constraint (사용자 정의 컬럼 제약조건)
+				
+				// partition table define
+				
+				// storage option
+				
+				// iot_type table define
+				
+				// table grant
+				
+				// table trigger
+				
+				// table synonyms 
+							
+				return result.toString();
+				
 			} else if(PublicTadpoleDefine.DB_ACTION.VIEWS == actionType) {
 				return ""+client.queryForObject("getViewScript", objectName);
 			}
