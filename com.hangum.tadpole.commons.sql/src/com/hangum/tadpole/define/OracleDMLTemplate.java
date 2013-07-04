cb7aeac1b6a12b55e4b707505c37ac8eb861648d
Hyun_Jong Cho <hangum@gmail.com>
2013-07-05 08:44:22 +0900

    https://github.com/hangum/TadpoleForDBTools/issues/185

--- com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/define/OracleDMLTemplate.java
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/define/OracleDMLTemplate.java
@@ -10,6 +10,8 @@
  ******************************************************************************/
 package com.hangum.tadpole.define;
 
+import com.hangum.tadpold.commons.libs.core.define.PublicTadpoleDefine;
+
 /**
  * Oracle DB의 디비를 정의합니다.
  * 
@@ -22,5 +24,10 @@ public class OracleDMLTemplate extends MySQLDMLTemplate {
 	// plan_table	
 	public static final String TMP_EXPLAIN_EXTENDED = "EXPLAIN PLAN INTO %s FOR ";
 
-	
+	/** procedure */
+	public static final String  TMP_CREATE_PROCEDURE_STMT = "CREATE or replace PROCEDURE simpleproc2 (param1 out INT) " + PublicTadpoleDefine.LINE_SEPARATOR +
+																	 " is " + PublicTadpoleDefine.LINE_SEPARATOR +
+																	  " BEGIN  " + PublicTadpoleDefine.LINE_SEPARATOR +
+																	  "  SELECT COUNT(*) INTO param1 FROM t;  " + PublicTadpoleDefine.LINE_SEPARATOR +
+																	  " END;";
 }
