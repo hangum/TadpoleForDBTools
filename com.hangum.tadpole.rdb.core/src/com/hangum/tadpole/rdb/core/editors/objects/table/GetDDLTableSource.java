0e040ec7aaacc94013f0f499f26530d0a56e3cfc
jeongjaehong <jeongjaehong@gmail.com>
2013-07-08 09:18:01 +0900

    MS SQL Server 테이블 DDL 스크립트 조회기능 추가.

--- com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/editors/objects/table/GetDDLTableSource.java
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/editors/objects/table/GetDDLTableSource.java
@@ -24,7 +24,6 @@ import com.hangum.tadpole.commons.sql.TadpoleSQLManager;
 import com.hangum.tadpole.commons.sql.define.DBDefine;
 import com.hangum.tadpole.dao.mysql.TriggerDAO;
 import com.hangum.tadpole.dao.system.UserDBDAO;
-import com.hangum.tadpole.rdb.core.util.FindEditorAndWriteQueryUtil;
 import com.ibatis.sqlmap.client.SqlMapClient;
 
 /**
@@ -58,7 +57,7 @@ public class GetDDLTableSource {
 			}
 			
 			throw new Exception("Not support Database");
-		} else if(DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.ORACLE_DEFAULT) {
+		} else if(DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.ORACLE_DEFAULT ) {
 			SqlMapClient client = TadpoleSQLManager.getInstance(userDB);
 			
 			if(PublicTadpoleDefine.DB_ACTION.TABLES == actionType) {
@@ -76,7 +75,7 @@ public class GetDDLTableSource {
 					if(i>0)result.append(",");
 					result.append(source.get("COLUMN_NAME")).append(" ");
 					result.append(source.get("DATA_TYPE"));
-					
+										
 					if (source.get("DATA_PRECISION") != null && ((BigDecimal)source.get("DATA_PRECISION")).intValue() > 0 ){
 						result.append("("+source.get("DATA_PRECISION"));
 						if (source.get("DATA_SCALE") != null && ((BigDecimal)source.get("DATA_SCALE")).intValue() > 0 ){
@@ -99,7 +98,7 @@ public class GetDDLTableSource {
 						}
 					}
 						
-					if("N".equals(source.get("NULLABLE"))){
+					if("NO".equals(source.get("NULLABLE"))){
 						result.append(" NOT NULL ");
 					}
 					
@@ -115,6 +114,128 @@ public class GetDDLTableSource {
 						result.append("\t,CONSTRAINT ").append(source.get("CONSTRAINT_NAME")).append(" PRIMARY KEY ( ").append(source.get("COLUMN_NAME"));
 					}else{
 						result.append(", "+source.get("COLUMN_NAME"));
+					}
+					
+					if(i == srcPkList.size()-1){
+						result.append(") \n");
+					}
+				}
+						
+				result.append("); \n\n");
+				
+				// table, column comments
+				List<String> srcCommentList = client.queryForList("getTableScript.comments", objectName);				
+				for (int i=0; i<srcCommentList.size(); i++){
+					result.append( srcCommentList.get(i)+"\n");
+				}
+				
+				// foreign key
+				
+				// column constraint (사용자 정의 컬럼 제약조건)
+				
+				// partition table define
+				
+				// storage option
+				
+				// iot_type table define
+				
+				// table grant
+				
+				// table trigger
+				
+				// table synonyms 
+							
+				return result.toString();
+				
+			} else if(PublicTadpoleDefine.DB_ACTION.VIEWS == actionType) {
+				//return ""+client.queryForObject("getViewScript", objectName);
+				
+				
+				StringBuilder result = new StringBuilder("");
+				result.append("/* DROP VIEW " + objectName + "; */ \n\n");
+
+				List<String> srcViewHeadList = client.queryForList("getViewScript.head", objectName);				
+				for (int i=0; i<srcViewHeadList.size(); i++){
+					result.append( srcViewHeadList.get(i)+"\n");
+				}
+				List<String> srcViewBodyList = client.queryForList("getViewScript.body", objectName);				
+				for (int i=0; i<srcViewBodyList.size(); i++){
+					result.append( srcViewBodyList.get(i)+"\n");
+				}
+				
+				return result.toString();				
+			} 
+			
+			throw new Exception("Not support Database");
+		} else if(DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.MSSQL_8_LE_DEFAULT ||
+				DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.MSSQL_DEFAULT ) {
+			SqlMapClient client = TadpoleSQLManager.getInstance(userDB);
+			
+			if(PublicTadpoleDefine.DB_ACTION.TABLES == actionType) {
+				List<HashMap> srcList = client.queryForList("getTableScript", objectName);
+				
+				
+				StringBuilder result = new StringBuilder("");
+				result.append("/* DROP TABLE " + objectName + " CASCADE CONSTRAINT; */ \n\n");
+				result.append("CREATE TABLE " + objectName + "( \n");
+				for (int i=0; i<srcList.size(); i++){
+					HashMap<String, Object> source =  srcList.get(i);
+					
+					
+					result.append("\t");
+					if(i>0)result.append(",");
+					result.append(source.get("COLUMN_NAME")).append(" ");
+					result.append(source.get("DATA_TYPE"));
+										
+					if (source.get("DATA_PRECISION") != null && ((Integer)source.get("DATA_PRECISION")).intValue() > 0 ){
+						result.append("("+source.get("DATA_PRECISION"));
+						if (source.get("DATA_SCALE") != null && ((Integer)source.get("DATA_SCALE")).intValue() > 0 ){
+							result.append(","+source.get("DATA_SCALE"));
+						}
+								
+						result.append(")");
+					}else if (!StringUtils.contains((String)source.get("DATA_TYPE"), "DATE") && !StringUtils.contains((String)source.get("DATA_TYPE"), "NUMBER")  && ((Integer)source.get("DATA_LENGTH")).intValue() > 0  ){
+						result.append("("+source.get("DATA_LENGTH")+")");
+					}else{
+						result.append(" ");
+					}
+					
+					if (source.get("DATA_DEFAULT") != null ){
+					
+						if (StringUtils.contains((String)source.get("DATA_TYPE"), "CHAR")  ){
+							result.append(" DEFAULT '"+(String)source.get("DATA_DEFAULT")+"'");
+						}else{
+							result.append(" DEFAULT "+(String)source.get("DATA_DEFAULT") );
+						}
+					}
+						
+					if("NO".equals(source.get("NULLABLE"))){
+						result.append(" NOT NULL ");
+					}
+					
+					result.append("\n");
+					
+				}
+
+				// primary key 
+				List<HashMap> srcPkList = client.queryForList("getTableScript.pk", objectName);				
+				for (int i=0; i<srcPkList.size(); i++){
+					HashMap<String, Object> source =  srcPkList.get(i);
+					if(i==0){
+						result.append("\t,CONSTRAINT ").append(source.get("CONSTRAINT_NAME")).append(" PRIMARY KEY ");
+						if ("CLUSTERED".equals(source.get("INDEX_TYPE")) ) {
+							result.append(" CLUSTERED ");
+						}
+						result.append(" ( ").append(source.get("COLUMN_NAME"));
+						
+						if ((Boolean)source.get("DESCENDING")){
+							result.append(" DESC ");
+						}
+					}else{
+						result.append(", "+source.get("COLUMN_NAME"));
+						if ((Boolean)source.get("DESCENDING")){
+							result.append(" DESC ");
+						}
 					}
 					
 					if(i == srcPkList.size()-1){
