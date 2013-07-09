e13f882037e8bf46e05aa0cf3b9e1946179ca99e
jeongjaehong <jeongjaehong@gmail.com>
2013-07-09 14:03:10 +0900

    테이블 컬럼 코멘트 수정시 코멘트 컬럼외의 컬럼영력이 수정가능으로 처리되는 오류 수정.

--- com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/viewers/object/sub/rdb/table/TadpoleTableComposite.java
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/viewers/object/sub/rdb/table/TadpoleTableComposite.java
@@ -329,7 +329,7 @@ public class TadpoleTableComposite extends AbstractObjectComposite {
 			tableColumn.getColumn().setText(name[i]);
 			tableColumn.getColumn().setWidth(size[i]);
 			tableColumn.getColumn().addSelectionListener(getSelectionAdapter(tableColumn, i));
-			tableColumn.setEditingSupport(new ColumnCommentEditorSupport(tableListViewer, tv, userDB, 3));
+			tableColumn.setEditingSupport(new ColumnCommentEditorSupport(tableListViewer, tv, userDB, i));
 		}
 	}
 	/**
