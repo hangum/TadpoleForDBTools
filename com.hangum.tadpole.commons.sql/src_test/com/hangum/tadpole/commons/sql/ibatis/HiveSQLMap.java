7bf54cd1afb43a13ce0798cfc287a6db4ab6e600
Hyun_Jong Cho <hangum@gmail.com>
2013-08-19 17:25:43 +0900

    hive

--- com.hangum.tadpole.commons.sql/src_test/com/hangum/tadpole/commons/sql/ibatis/HiveSQLMap.java
+++ com.hangum.tadpole.commons.sql/src_test/com/hangum/tadpole/commons/sql/ibatis/HiveSQLMap.java
@@ -31,28 +31,28 @@ public class HiveSQLMap {
 
 	public static void main(String[] args) {
 		try {
-//			SqlMapClient client = SQLMap.getInstance(getUserDB());
-//			 Connection con = client.getDataSource().getConnection();
-//			
-//			 Statement stmt = con.createStatement();
-//			 String tableName = "testHiveDriverTable";
-//			 stmt.executeQuery("drop table " + tableName);
-//			 ResultSet res = stmt.executeQuery("create table " + tableName +
-//			 " (key int, value string)");
-//			 // show tables
-//			 String sql = "show tables '" + tableName + "'";
-//			 System.out.println("Running: " + sql);
-//			 res = stmt.executeQuery(sql);
-//			 if (res.next()) {
-//				 System.out.println(res.getString(1));
-//			 }
-//			 // describe table
-//			 sql = "describe " + tableName;
-//			 System.out.println("Running: " + sql);
-//			 res = stmt.executeQuery(sql);
-//			 while (res.next()) {
-//				 System.out.println(res.getString(1) + "\t" + res.getString(2));
-//			 }
+			SqlMapClient client = SQLMap.getInstance(getUserDB());
+			 Connection con = client.getDataSource().getConnection();
+			
+			 Statement stmt = con.createStatement();
+			 String tableName = "testHiveDriverTable";
+			 stmt.executeQuery("drop table " + tableName);
+			 ResultSet res = stmt.executeQuery("create table " + tableName +
+			 " (key int, value string)");
+			 // show tables
+			 String sql = "show tables '" + tableName + "'";
+			 System.out.println("Running: " + sql);
+			 res = stmt.executeQuery(sql);
+			 if (res.next()) {
+				 System.out.println(res.getString(1));
+			 }
+			 // describe table
+			 sql = "describe " + tableName;
+			 System.out.println("Running: " + sql);
+			 res = stmt.executeQuery(sql);
+			 while (res.next()) {
+				 System.out.println(res.getString(1) + "\t" + res.getString(2));
+			 }
 
 			ibatisSelect();
 
