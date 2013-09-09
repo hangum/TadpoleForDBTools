95331054052771ec6e62765a79afafd38daea6f4
Hyun_Jong Cho <hangum@gmail.com>
2013-09-10 01:40:45 +0900

    code refactoring

--- com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/commons/sql/Messages.java
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/commons/sql/Messages.java
@@ -15,6 +15,13 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "com.hangum.db.commons.sql.messages"; //$NON-NLS-1$
 	public static String TadpoleSQLManager_0;
+	public static String SQLHistoryCreateColumn_0;
+	public static String SQLHistoryCreateColumn_1;
+	public static String SQLHistoryCreateColumn_2;
+	public static String SQLHistoryCreateColumn_3;
+	public static String SQLHistoryCreateColumn_4;
+	public static String SQLHistoryCreateColumn_5;
+	
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
