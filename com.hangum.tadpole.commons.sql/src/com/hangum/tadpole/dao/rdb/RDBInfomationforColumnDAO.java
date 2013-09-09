6f97200558c95aed8070186cf6844daf7c4ab0b0
Hyun_Jong Cho <hangum@gmail.com>
2013-09-10 01:36:23 +0900

    Change targetplatform 2.1.1-RC3

---
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/dao/rdb/RDBInfomationforColumnDAO.java
@@ -0,0 +1,309 @@
+/*******************************************************************************
+ * Copyright (c) 2013 hangum.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     hangum - initial API and implementation
+ *     nilriri - RDBMS information for columns.
+ ******************************************************************************/
+package com.hangum.tadpole.dao.rdb;
+
+/**
+ * 테이블의 모든 컬럼에 대한 정보를 조회
+ * 
+ * <pre>
+ * 		조회 권한을 갖는 모든 테이블에 대한 컬럼 목록을 표시하여 컬럼의 통계정보 생성유무나 데이터 자료형의 불일치, 코멘드나 참조무결성 정의 여부들을 한 화면에서 조회할 수 있도록 한다.
+ * 
+ * </pre>
+ * 
+ * @author nilriri
+ * 
+ */
+
+public class RDBInfomationforColumnDAO {
+
+	private String table_name;
+	private String table_comment;
+	private String column_name;
+	private String nullable;
+	private String data_type;
+	private String data_default;
+	private String column_comment;
+	private String data_type_mod;
+	private String char_used;
+	private String histogram;
+	private String num_distinct;
+	private String num_nulls;
+	private String density;
+	private String last_analyzed;
+
+	/**
+	 * 
+	 */
+	public RDBInfomationforColumnDAO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int compareToIgnoreCase(RDBInfomationforColumnDAO target, String column) {
+
+		String value1 = "";
+		String value2 = "";
+		if (column != null && !"".equals(column)) {
+
+			value1 = (String) this.getColumnValuebyName(column);
+			value2 = (String) target.getColumnValuebyName(column);
+		}
+		return value1.compareToIgnoreCase(value2);
+
+	}
+
+	public String getColumnValuebyName(String column) {
+		String result = "";
+		if ("table_name".equals(column.toLowerCase())) {
+			result = this.getTable_name();
+		} else if ("table_comment".equals(column.toLowerCase())) {
+			result = this.getTable_comment();
+		} else if ("column_name".equals(column.toLowerCase())) {
+			result = this.getColumn_name();
+		} else if ("nullable".equals(column.toLowerCase())) {
+			result = this.getNullable();
+		} else if ("data_type".equals(column.toLowerCase())) {
+			result = this.getData_type();
+		} else if ("data_default".equals(column.toLowerCase())) {
+			result = this.getData_default();
+		} else if ("column_comment".equals(column.toLowerCase())) {
+			result = this.getColumn_comment();
+		} else if ("data_type_mod".equals(column.toLowerCase())) {
+			result = this.getData_type_mod();
+		} else if ("char_used".equals(column.toLowerCase())) {
+			result = this.getChar_used();
+		} else if ("histogram".equals(column.toLowerCase())) {
+			result = this.getHistogram();
+		} else if ("num_distinct".equals(column.toLowerCase())) {
+			result = this.getNum_distinct();
+		} else if ("num_nulls".equals(column.toLowerCase())) {
+			result = this.getNum_nulls();
+		} else if ("density".equals(column.toLowerCase())) {
+			result = this.getDensity();
+		} else if ("last_analyzed".equals(column.toLowerCase())) {
+			result = this.getLast_analyzed();
+		} else {
+			result = "";
+		}
+		return result == null ? "" : result;
+	}
+
+	/**
+	 * @return the table_name
+	 */
+	public String getTable_name() {
+		return table_name;
+	}
+
+	/**
+	 * @param table_name
+	 *            the table_name to set
+	 */
+	public void setTable_name(String table_name) {
+		this.table_name = table_name;
+	}
+
+	/**
+	 * @return the table_comment
+	 */
+	public String getTable_comment() {
+		return table_comment;
+	}
+
+	/**
+	 * @param table_comment
+	 *            the table_comment to set
+	 */
+	public void setTable_comment(String table_comment) {
+		this.table_comment = table_comment;
+	}
+
+	/**
+	 * @return the column_name
+	 */
+	public String getColumn_name() {
+		return column_name;
+	}
+
+	/**
+	 * @param column_name
+	 *            the column_name to set
+	 */
+	public void setColumn_name(String column_name) {
+		this.column_name = column_name;
+	}
+
+	/**
+	 * @return the nullable
+	 */
+	public String getNullable() {
+		return nullable;
+	}
+
+	/**
+	 * @param nullable
+	 *            the nullable to set
+	 */
+	public void setNullable(String nullable) {
+		this.nullable = nullable;
+	}
+
+	/**
+	 * @return the data_type
+	 */
+	public String getData_type() {
+		return data_type;
+	}
+
+	/**
+	 * @param data_type
+	 *            the data_type to set
+	 */
+	public void setData_type(String data_type) {
+		this.data_type = data_type;
+	}
+
+	/**
+	 * @return the data_default
+	 */
+	public String getData_default() {
+		return data_default;
+	}
+
+	/**
+	 * @param data_default
+	 *            the data_default to set
+	 */
+	public void setData_default(String data_default) {
+		this.data_default = data_default;
+	}
+
+	/**
+	 * @return the column_comment
+	 */
+	public String getColumn_comment() {
+		return column_comment;
+	}
+
+	/**
+	 * @param column_comment
+	 *            the column_comment to set
+	 */
+	public void setColumn_comment(String column_comment) {
+		this.column_comment = column_comment;
+	}
+
+	/**
+	 * @return the data_type_mod
+	 */
+	public String getData_type_mod() {
+		return data_type_mod;
+	}
+
+	/**
+	 * @param data_type_mod
+	 *            the data_type_mod to set
+	 */
+	public void setData_type_mod(String data_type_mod) {
+		this.data_type_mod = data_type_mod;
+	}
+
+	/**
+	 * @return the char_used
+	 */
+	public String getChar_used() {
+		return char_used;
+	}
+
+	/**
+	 * @param char_used
+	 *            the char_used to set
+	 */
+	public void setChar_used(String char_used) {
+		this.char_used = char_used;
+	}
+
+	/**
+	 * @return the histogram
+	 */
+	public String getHistogram() {
+		return histogram;
+	}
+
+	/**
+	 * @param histogram
+	 *            the histogram to set
+	 */
+	public void setHistogram(String histogram) {
+		this.histogram = histogram;
+	}
+
+	/**
+	 * @return the num_distinct
+	 */
+	public String getNum_distinct() {
+		return num_distinct;
+	}
+
+	/**
+	 * @param num_distinct
+	 *            the num_distinct to set
+	 */
+	public void setNum_distinct(String num_distinct) {
+		this.num_distinct = num_distinct;
+	}
+
+	/**
+	 * @return the num_nulls
+	 */
+	public String getNum_nulls() {
+		return num_nulls;
+	}
+
+	/**
+	 * @param num_nulls
+	 *            the num_nulls to set
+	 */
+	public void setNum_nulls(String num_nulls) {
+		this.num_nulls = num_nulls;
+	}
+
+	/**
+	 * @return the density
+	 */
+	public String getDensity() {
+		return density;
+	}
+
+	/**
+	 * @param density
+	 *            the density to set
+	 */
+	public void setDensity(String density) {
+		this.density = density;
+	}
+
+	/**
+	 * @return the last_analyzed
+	 */
+	public String getLast_analyzed() {
+		return last_analyzed;
+	}
+
+	/**
+	 * @param last_analyzed
+	 *            the last_analyzed to set
+	 */
+	public void setLast_analyzed(String last_analyzed) {
+		this.last_analyzed = last_analyzed;
+	}
+
+}
---
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/editors/dbinfos/composites/ColumnsComposite.java
@@ -0,0 +1,369 @@
+/*******************************************************************************
+ * Copyright (c) 2013 hangum.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     hangum - initial API and implementation
+ *     nilriri - column information.
+ ******************************************************************************/
+package com.hangum.tadpole.rdb.core.editors.dbinfos.composites;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.Text;
+
+import com.hangum.tadpole.commons.sql.TadpoleSQLManager;
+import com.hangum.tadpole.commons.sql.define.DBDefine;
+import com.hangum.tadpole.dao.rdb.RDBInfomationforColumnDAO;
+import com.hangum.tadpole.dao.system.UserDBDAO;
+import com.hangum.tadpole.exception.dialog.ExceptionDetailsErrorDialog;
+import com.hangum.tadpole.rdb.core.Activator;
+import com.hangum.tadpole.rdb.core.Messages;
+import com.hangum.tadpole.rdb.core.viewers.object.comparator.ObjectComparator;
+import com.ibatis.sqlmap.client.SqlMapClient;
+
+/**
+ * Tables composite
+ * 
+ * @author nilriri
+ * 
+ */
+public class ColumnsComposite extends Composite {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(ColumnsComposite.class);
+
+	private UserDBDAO userDB;
+	private TableViewer tvColumnInform;
+
+	private ColumnInfoFilter columnFilter;
+	private Text textFilter;
+
+	/**
+	 * Create the composite.
+	 * 
+	 * @param parent
+	 * @param style
+	 */
+	public ColumnsComposite(Composite parent, int style, UserDBDAO userDB) {
+		super(parent, style);
+		setLayout(new GridLayout(1, false));
+
+		this.userDB = userDB;
+
+		Composite compositeHead = new Composite(this, SWT.NONE);
+		compositeHead.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		compositeHead.setLayout(new GridLayout(3, false));
+
+		Label lblNewLabel = new Label(compositeHead, SWT.NONE);
+		lblNewLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lblNewLabel.setText("Filter");
+
+		textFilter = new Text(compositeHead, SWT.SEARCH | SWT.ICON_SEARCH | SWT.ICON_CANCEL);
+		textFilter.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		textFilter.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.keyCode == SWT.Selection) {
+					columnFilter.setSearchString(textFilter.getText());
+					tvColumnInform.refresh();
+				}
+			}
+		});
+
+		Button btnRefresh = new Button(compositeHead, SWT.NONE);
+		btnRefresh.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				initUI();
+			}
+		});
+		btnRefresh.setText("Refresh");
+
+		tvColumnInform = new TableViewer(this, SWT.BORDER | SWT.FULL_SELECTION);
+		Table table = tvColumnInform.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+
+		createTableColumn();
+
+		columnFilter = new ColumnInfoFilter();
+		tvColumnInform.addFilter(columnFilter);
+
+		initUI();
+	}
+
+	/**
+	 * table column head를 생성합니다.
+	 */
+	private void createTableColumn() {
+
+		TableColumnDef[] tableColumnDef = new TableColumnDef[] {};
+		if (DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.MYSQL_DEFAULT || DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.MARIADB_DEFAULT) {
+
+			tableColumnDef = new TableColumnDef[] { //
+			new TableColumnDef("TABLE_NAME", "Table Name", 100, SWT.LEFT) //
+					, new TableColumnDef("TABLE_COMMENT", "Table Comment", 100, SWT.LEFT) //
+					, new TableColumnDef("COLUMN_NAME", "Column Name", 100, SWT.LEFT) //
+					, new TableColumnDef("NULLABLE", "Nullable", 100, SWT.LEFT) //
+					, new TableColumnDef("DATA_TYPE", "Data Type", 100, SWT.LEFT) //
+					, new TableColumnDef("DATA_DEFAULT", "Data Default", 100, SWT.LEFT) //
+					, new TableColumnDef("COLUMN_COMMENT", "Column Comment", 100, SWT.LEFT) //
+					, new TableColumnDef("DATA_TYPE_MOD", "Data Type Mod", 100, SWT.LEFT) //
+					, new TableColumnDef("CHAR_USED", "Char Used", 100, SWT.LEFT) //
+					, new TableColumnDef("HISTOGRAM", "Histogram", 100, SWT.LEFT) //
+					, new TableColumnDef("NUM_DISTINCT", "Num Distinct", 100, SWT.LEFT) //
+					, new TableColumnDef("NUM_NULLS", "Num Nulls", 100, SWT.LEFT) //
+					, new TableColumnDef("DENSITY", "Density", 100, SWT.LEFT) //
+					, new TableColumnDef("LAST_ANALYZED", "Last Analyzed", 100, SWT.LEFT) //
+			};
+
+		} else if (DBDefine.getDBDefine(userDB.getDbms_types()) == DBDefine.ORACLE_DEFAULT) {
+
+			tableColumnDef = new TableColumnDef[] { //
+			new TableColumnDef("TABLE_NAME", "Table Name", 100, SWT.LEFT) //
+					, new TableColumnDef("TABLE_COMMENT", "Table Comment", 150, SWT.LEFT) //
+					, new TableColumnDef("COLUMN_NAME", "Column Name", 120, SWT.LEFT) //
+					, new TableColumnDef("COLUMN_COMMENT", "Column Comment", 150, SWT.LEFT) //
+					, new TableColumnDef("NULLABLE", "Nullable", 60, SWT.CENTER) //
+					, new TableColumnDef("DATA_TYPE", "Data Type", 120, SWT.LEFT) //
+					, new TableColumnDef("DATA_DEFAULT", "Data Default", 100, SWT.LEFT) //
+					, new TableColumnDef("DATA_TYPE_MOD", "Data Type Mod", 100, SWT.LEFT) //
+					, new TableColumnDef("CHAR_USED", "Char Used", 60, SWT.CENTER) //
+					, new TableColumnDef("HISTOGRAM", "Histogram", 60, SWT.CENTER) //
+					, new TableColumnDef("NUM_DISTINCT", "Num Distinct", 100, SWT.RIGHT) //
+					, new TableColumnDef("NUM_NULLS", "Num Nulls", 100, SWT.RIGHT) //
+					, new TableColumnDef("DENSITY", "Density", 100, SWT.RIGHT) //
+					, new TableColumnDef("LAST_ANALYZED", "Last Analyzed", 120, SWT.LEFT) //
+			};
+
+		} else {
+			tableColumnDef = new TableColumnDef[] { //
+			new TableColumnDef("TABLE_NAME", "Table Name", 100, SWT.LEFT) //
+					, new TableColumnDef("TABLE_COMMENT", "Table Comment", 100, SWT.LEFT) //
+					, new TableColumnDef("COLUMN_NAME", "Column Name", 100, SWT.LEFT) //
+					, new TableColumnDef("NULLABLE", "Nullable", 100, SWT.LEFT) //
+					, new TableColumnDef("DATA_TYPE", "Data Type", 100, SWT.LEFT) //
+					, new TableColumnDef("DATA_DEFAULT", "Data Default", 100, SWT.LEFT) //
+					, new TableColumnDef("COLUMN_COMMENT", "Column Comment", 100, SWT.LEFT) //
+					, new TableColumnDef("DATA_TYPE_MOD", "Data Type Mod", 100, SWT.LEFT) //
+					, new TableColumnDef("CHAR_USED", "Char Used", 100, SWT.LEFT) //
+					, new TableColumnDef("HISTOGRAM", "Histogram", 100, SWT.LEFT) //
+					, new TableColumnDef("NUM_DISTINCT", "Num Distinct", 100, SWT.LEFT) //
+					, new TableColumnDef("NUM_NULLS", "Num Nulls", 100, SWT.LEFT) //
+					, new TableColumnDef("DENSITY", "Density", 100, SWT.LEFT) //
+					, new TableColumnDef("LAST_ANALYZED", "Last Analyzed", 100, SWT.LEFT) //
+			};
+		}
+
+		createColumnHeader(tableColumnDef);
+
+		tvColumnInform.setContentProvider(new ArrayContentProvider());
+		tvColumnInform.setLabelProvider(new ColumnInformLabelProvider(tvColumnInform));
+
+	}
+
+	/**
+	 * 실제 컬럼의 헤더를 생성합니다.
+	 * 
+	 * @param name
+	 * @param size
+	 */
+	private void createColumnHeader(TableColumnDef[] colDef) {
+		AllColumnComparator tableComparator = new AllColumnComparator();
+
+		for (int i = 0; i < colDef.length; i++) {
+			TableViewerColumn tableViewerColumn = new TableViewerColumn(tvColumnInform, colDef[i].align);
+
+			TableColumn tableColumn = tableViewerColumn.getColumn();
+			tableColumn.setText(colDef[i].caption);
+			tableColumn.setData("column", colDef[i].column);
+			tableColumn.setWidth(colDef[i].width);
+
+			tableColumn.addSelectionListener(getSelectionAdapter(tvColumnInform, tableComparator, tableColumn, i));
+
+		}
+
+		tvColumnInform.setSorter(tableComparator);
+	}
+
+	protected SelectionAdapter getSelectionAdapter(final TableViewer viewer, final ObjectComparator comparator, final TableColumn column, final int index) {
+		SelectionAdapter adapter = new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				comparator.setColumn(index);
+				viewer.getTable().setSortDirection(comparator.getDirection());
+				viewer.getTable().setSortColumn(column);
+				viewer.refresh();
+			}
+		};
+
+		return adapter;
+	}
+
+	/**
+	 * 
+	 */
+	private void initUI() {
+		try {
+			SqlMapClient sqlClient = TadpoleSQLManager.getInstance(userDB);
+			List<RDBInfomationforColumnDAO> listTableInform = sqlClient.queryForList("columnInformation", userDB.getDb()); //$NON-NLS-1$ //$NON-NLS-2$
+
+			tvColumnInform.setInput(listTableInform);
+			tvColumnInform.refresh();
+		} catch (Exception e) {
+			logger.error("initialize column summary", e);
+
+			Status errStatus = new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e); //$NON-NLS-1$
+			ExceptionDetailsErrorDialog.openError(null, "Error", Messages.MainEditor_19, errStatus); //$NON-NLS-1$
+		}
+	}
+
+	@Override
+	protected void checkSubclass() {
+	}
+
+}
+
+class TableColumnDef {
+	String column;
+	String caption;
+	int width;
+	int align;
+
+	public TableColumnDef(String column) {
+		this(column, StringUtils.capitalize(column.toLowerCase().replace("_", "")));
+	}
+
+	public TableColumnDef(String column, String caption) {
+		this(column, caption, 100);
+	}
+
+	public TableColumnDef(String column, String caption, int width) {
+		this(column, caption, width, SWT.LEFT);
+	}
+
+	TableColumnDef(String column, String caption, int width, int align) {
+		this.column = column;
+		this.caption = caption == null ? StringUtils.capitalize(column.toLowerCase().replace("_", "")) : caption;
+		this.width = width == 0 ? 100 : width;
+		this.align = align <= 0 ? SWT.LEFT : align;
+	}
+}
+
+/**
+ * sort를 위한 최상위 클래서(기본으로 table의 column 사용)
+ * 
+ * @author hangum
+ * 
+ */
+class AllColumnComparator extends ObjectComparator {
+	public AllColumnComparator() {
+		this.propertyIndex = 0;
+		direction = DESCENDING;
+	}
+
+	@Override
+	public int compare(Viewer viewer, Object e1, Object e2) {
+		RDBInfomationforColumnDAO tc1 = (RDBInfomationforColumnDAO) e1;
+		RDBInfomationforColumnDAO tc2 = (RDBInfomationforColumnDAO) e2;
+
+		String column = (String) ((TableViewer) viewer).getTable().getColumn(propertyIndex).getData("column");
+		int rc = tc1.compareToIgnoreCase(tc2, column);
+
+		if (direction == DESCENDING) {
+			rc = -rc;
+		}
+		return rc;
+	}
+}
+
+/**
+ * mysql, mariadb label provider
+ * 
+ * @author hangum
+ * 
+ */
+class ColumnInformLabelProvider extends LabelProvider implements ITableLabelProvider {
+	private TableViewer tableViewer;
+
+	public ColumnInformLabelProvider(TableViewer tv) {
+		this.tableViewer = tv;
+	}
+
+	@Override
+	public Image getColumnImage(Object element, int columnIndex) {
+		return null;
+	}
+
+	@Override
+	public String getColumnText(Object element, int columnIndex) {
+		String columnName = (String) tableViewer.getTable().getColumn(columnIndex).getData("column");
+		RDBInfomationforColumnDAO resultMap = (RDBInfomationforColumnDAO) element;
+		return resultMap.getColumnValuebyName(columnName);
+	}
+
+}
+
+/**
+ * name filetr
+ * 
+ * @author hangum
+ * 
+ */
+class ColumnInfoFilter extends ViewerFilter {
+	private String searchString;
+
+	public void setSearchString(String s) {
+		this.searchString = ".*" + s.toLowerCase() + ".*"; //$NON-NLS-1$ //$NON-NLS-2$
+	}
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		if (searchString == null || searchString.length() == 0) {
+			return true;
+		} else {
+
+			RDBInfomationforColumnDAO dao = (RDBInfomationforColumnDAO) element;
+			String targetString = "";
+
+			for (TableColumn tc : ((TableViewer) viewer).getTable().getColumns()) {
+				targetString = dao.getColumnValuebyName((String) tc.getData("column")).toLowerCase();
+
+				if (targetString.matches(searchString)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
