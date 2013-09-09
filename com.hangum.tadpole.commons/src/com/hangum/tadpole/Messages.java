e61f6e44671888bfd0f7648c395b380e4c59bba0
Hyun_Jong Cho <hangum@gmail.com>
2013-09-10 01:39:21 +0900

    code refactoring

--- com.hangum.tadpole.commons/src/com/hangum/tadpole/Messages.java
+++ com.hangum.tadpole.commons/src/com/hangum/tadpole/Messages.java
@@ -10,12 +10,7 @@ public class Messages extends NLS {
 	public static String FileUploadDialog_4;
 	public static String FileUploadDialog_6;
 	public static String FileUploadDialog_8;
-	public static String SQLHistoryCreateColumn_0;
-	public static String SQLHistoryCreateColumn_1;
-	public static String SQLHistoryCreateColumn_2;
-	public static String SQLHistoryCreateColumn_3;
-	public static String SQLHistoryCreateColumn_4;
-	public static String SQLHistoryCreateColumn_5;
+	
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
