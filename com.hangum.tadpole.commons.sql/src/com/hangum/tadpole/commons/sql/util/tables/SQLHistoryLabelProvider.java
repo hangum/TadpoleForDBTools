c3fff6a04b7f1cc1eaa722e82368bbdcdaba854c
Hyun_Jong Cho <hangum@gmail.com>
2013-09-10 01:40:11 +0900

    code refactoring

---
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/commons/sql/util/tables/SQLHistoryLabelProvider.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2013 hangum.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     hangum - initial API and implementation
+ ******************************************************************************/
+package com.hangum.tadpole.commons.sql.util.tables;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+
+import com.hangum.tadpole.dialogs.message.dao.SQLHistoryDAO;
+import com.hangum.tadpole.dialogs.message.dao.TadpoleMessageDAO;
+
+/**
+ * history label provider
+ * @author hangum
+ *
+ */
+public class SQLHistoryLabelProvider extends LabelProvider implements ITableLabelProvider {
+	
+	@Override
+	public Image getColumnImage(Object element, int columnIndex) {
+		return null;
+	}
+
+	@Override
+	public String getColumnText(Object element, int columnIndex) {
+		if(element instanceof SQLHistoryDAO) {
+			SQLHistoryDAO historyDAO = (SQLHistoryDAO)element;
+			
+			switch(columnIndex) {
+				case 0: return dateToStr(historyDAO.getStartDateExecute());
+				// 쿼리에 개행 문자가 있으면 테이블에 개행 문자 이후 쿼리가 보이지 않으므로 보여줄 때는 개행 문자를 제거합니다.
+				case 1: return StringUtils.replaceChars(historyDAO.getStrSQLText(), "\n", " ");
+				case 2: 
+					return ""+(historyDAO.getEndDateExecute().getTime() - historyDAO.getStartDateExecute().getTime());
+				case 3: return ""+historyDAO.getRows();
+				case 4: return historyDAO.getResult();
+				case 5: return historyDAO.getMesssage();
+			}
+		} else if(element instanceof TadpoleMessageDAO) {
+			TadpoleMessageDAO messageDAO = (TadpoleMessageDAO)element;
+			
+			switch(columnIndex) {
+				case 0: return dateToStr(messageDAO.getDateExecute());
+				case 1: return messageDAO.getStrMessage();
+			}
+		}
+		
+		return "### not set column ###"; //$NON-NLS-1$
+	}
+
+	/**
+	 * 보기편하게
+	 * 
+	 * @param date
+	 * @return
+	 */
+	public static String dateToStr(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return sdf.format(date);
+	}
+}
