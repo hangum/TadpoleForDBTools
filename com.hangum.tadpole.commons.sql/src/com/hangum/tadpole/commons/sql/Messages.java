ebfbde181c169925641af66d68fb089c04b672e6
Hyun_Jong Cho <hangum@gmail.com>
2013-09-16 07:06:54 +0900

    fix localization

--- com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/commons/sql/Messages.java
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/commons/sql/Messages.java
@@ -13,7 +13,7 @@ package com.hangum.tadpole.commons.sql;
 import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
-	private static final String BUNDLE_NAME = "com.hangum.db.commons.sql.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.hangum.tadpole.commons.sql.messages"; //$NON-NLS-1$
 	public static String TadpoleSQLManager_0;
 	public static String SQLHistoryCreateColumn_0;
 	public static String SQLHistoryCreateColumn_1;
