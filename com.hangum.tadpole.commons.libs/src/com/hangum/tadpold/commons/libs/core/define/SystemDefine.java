58220a859ada4af8ff478cade757a952cee90d25
Hyun_Jong Cho <hangum@gmail.com>
2013-11-19 14:34:08 +0900

    Modify System version number

--- com.hangum.tadpole.commons.libs/src/com/hangum/tadpold/commons/libs/core/define/SystemDefine.java
+++ com.hangum.tadpole.commons.libs/src/com/hangum/tadpold/commons/libs/core/define/SystemDefine.java
@@ -20,9 +20,9 @@ public class SystemDefine {
 	
 	public static final String NAME = "Tadpole DB Hub";
 	public static final String MAJOR_VERSION = "1.1.3(Living)";
-	public static final String SUB_VERSION = "Build" + "(05)";	
-	public static final String RELEASE_DATE = "2013.11.13";
-	public static final String INFORMATION = "https://github.com/hangum/TadpoleForDBTools";
+	public static final String SUB_VERSION = "Build" + "(06)";	
+	public static final String RELEASE_DATE = "2013.11.19";
+	public static final String INFORMATION = "https://github.com/hangum/TadpoleForDBTools/wiki";
 	
 	public static final String ADMIN_EMAIL = "adi.tadpole@gmail.com";
 	public static final String SOURCE_PAGE = INFORMATION;
