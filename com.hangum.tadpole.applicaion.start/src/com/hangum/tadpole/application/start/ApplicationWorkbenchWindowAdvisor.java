e85167de550fb8fe5a9fc9b7549b757d82b72114
Hyun_Jong Cho <hangum@gmail.com>
2013-12-09 18:26:30 +0900

    I die

--- com.hangum.tadpole.applicaion.start/src/com/hangum/tadpole/application/start/ApplicationWorkbenchWindowAdvisor.java
+++ com.hangum.tadpole.applicaion.start/src/com/hangum/tadpole/application/start/ApplicationWorkbenchWindowAdvisor.java
@@ -133,7 +133,7 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 					    } // end try
     				
 	    				try {
-							Thread.sleep(10 * 1500);								
+							Thread.sleep(15 * 1000);
 	    				} catch(Exception e){}
     				}
     			}	// end while
