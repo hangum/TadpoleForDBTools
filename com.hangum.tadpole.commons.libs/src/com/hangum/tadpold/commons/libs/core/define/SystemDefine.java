03d42b1c2c814fd973bbd962ca9231c8329e4202
Hyun_Jong Cho <hangum@gmail.com>
2014-03-14 02:46:48 +0900

    Change version number. v1.3.1 started

--- com.hangum.tadpole.commons.libs/src/com/hangum/tadpold/commons/libs/core/define/SystemDefine.java
+++ com.hangum.tadpole.commons.libs/src/com/hangum/tadpold/commons/libs/core/define/SystemDefine.java
@@ -19,7 +19,7 @@ package com.hangum.tadpold.commons.libs.core.define;
 public class SystemDefine {
 	
 	public static final String NAME = "Tadpole DB Hub";
-	public static final String MAJOR_VERSION = "1.3.0 (Living)";
+	public static final String MAJOR_VERSION = "1.3.1 (edge)";
 	public static final String SUB_VERSION = "Build " + "(R0)";	
 	public static final String RELEASE_DATE = "2014.03.07";
 	public static final String INFORMATION = "https://github.com/hangum/TadpoleForDBTools/wiki";
