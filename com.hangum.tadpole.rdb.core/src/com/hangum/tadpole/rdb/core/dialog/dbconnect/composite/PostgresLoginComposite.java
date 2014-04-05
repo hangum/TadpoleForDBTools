da67143c892f214e809a651a08e6c929779ace28
Hyun_Jong Cho <hangum@gmail.com>
2014-04-06 01:01:13 +0900

    pgsql is support ssl.

--- com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/dialog/dbconnect/composite/PostgresLoginComposite.java
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/dialog/dbconnect/composite/PostgresLoginComposite.java
@@ -13,13 +13,28 @@ package com.hangum.tadpole.rdb.core.dialog.dbconnect.composite;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
 import com.hangum.tadpold.commons.libs.core.define.PublicTadpoleDefine;
 import com.hangum.tadpole.commons.util.ApplicationArgumentUtils;
 import com.hangum.tadpole.engine.define.DBDefine;
+import com.hangum.tadpole.rdb.core.Messages;
+import com.hangum.tadpole.rdb.core.dialog.dbconnect.sub.PreConnectionInfoGroup;
+import com.hangum.tadpole.rdb.core.dialog.dbconnect.sub.others.OthersConnectionRDBGroup;
 import com.hangum.tadpole.rdb.core.dialog.dbconnect.sub.others.dao.OthersConnectionInfoDAO;
+import com.hangum.tadpole.rdb.core.util.DBLocaleUtils;
 import com.hangum.tadpole.sql.dao.system.UserDBDAO;
 import com.hangum.tadpole.sql.session.manager.SessionManager;
 import com.hangum.tadpole.sql.template.DBOperationType;
@@ -31,8 +46,8 @@ import com.hangum.tadpole.sql.template.DBOperationType;
  *
  */
 public class PostgresLoginComposite extends MySQLLoginComposite {
-
-//	private static final Logger logger = Logger.getLogger(PostgresLoginComposite.class);
+	private static final Logger logger = Logger.getLogger(PostgresLoginComposite.class);
+	protected Combo comboSSL;
 	
 	/**
 	 * Create the composite.
@@ -41,6 +56,111 @@ public class PostgresLoginComposite extends MySQLLoginComposite {
 	 */
 	public PostgresLoginComposite(Composite parent, int style, List<String> listGroupName, String selGroupName, UserDBDAO userDB) {
 		super("Sample PostgreSQL", DBDefine.POSTGRE_DEFAULT, parent, style, listGroupName, selGroupName, userDB);
+	}
+	
+	@Override
+	public void crateComposite() {
+		GridLayout gridLayout = new GridLayout(1, false);
+		gridLayout.verticalSpacing = 2;
+		gridLayout.horizontalSpacing = 2;
+		gridLayout.marginHeight = 2;
+		gridLayout.marginWidth = 0;
+		setLayout(gridLayout);
+		setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+
+		Composite compositeBody = new Composite(this, SWT.NONE);
+		GridLayout gl_compositeBody = new GridLayout(1, false);
+		gl_compositeBody.verticalSpacing = 2;
+		gl_compositeBody.horizontalSpacing = 2;
+		gl_compositeBody.marginHeight = 2;
+		gl_compositeBody.marginWidth = 2;
+		compositeBody.setLayout(gl_compositeBody);
+		compositeBody.setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, false, 1, 1));
+		
+		preDBInfo = new PreConnectionInfoGroup(compositeBody, SWT.NONE, listGroupName);
+		preDBInfo.setText(Messages.MSSQLLoginComposite_preDBInfo_text);
+		preDBInfo.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1));
+		
+		Group grpConnectionType = new Group(compositeBody, SWT.NONE);
+		grpConnectionType.setLayout(new GridLayout(3, false));
+		grpConnectionType.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		grpConnectionType.setText(Messages.MSSQLLoginComposite_grpConnectionType_text);
+		
+		Label lblHost = new Label(grpConnectionType, SWT.NONE);
+		lblHost.setText(Messages.DBLoginDialog_1);
+		
+		textHost = new Text(grpConnectionType, SWT.BORDER);
+		textHost.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		
+		Label lblNewLabelPort = new Label(grpConnectionType, SWT.NONE);
+		lblNewLabelPort.setText(Messages.DBLoginDialog_5);
+		
+		textPort = new Text(grpConnectionType, SWT.BORDER);
+		textPort.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Button btnPing = new Button(grpConnectionType, SWT.NONE);
+		btnPing.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				String host 	= StringUtils.trimToEmpty(textHost.getText());
+				String port 	= StringUtils.trimToEmpty(textPort.getText());
+				
+				if("".equals(host) || "".equals(port)) { //$NON-NLS-1$ //$NON-NLS-2$
+					MessageDialog.openError(null, Messages.DBLoginDialog_10, Messages.DBLoginDialog_11);
+					return;
+				}
+				
+				try {
+					if(isPing(host, port)) {
+						MessageDialog.openInformation(null, Messages.DBLoginDialog_12, Messages.DBLoginDialog_13);
+					} else {
+						MessageDialog.openError(null, Messages.DBLoginDialog_14, Messages.DBLoginDialog_15);
+					}
+				} catch(NumberFormatException nfe) {
+					MessageDialog.openError(null, Messages.MySQLLoginComposite_3, Messages.MySQLLoginComposite_4);
+				}
+			}
+		});
+		btnPing.setText(Messages.DBLoginDialog_btnPing_text);
+		
+		Label lblNewLabelDatabase = new Label(grpConnectionType, SWT.NONE);
+		lblNewLabelDatabase.setLayoutData(new GridData(SWT.LEFT, SWT.BOTTOM, false, false, 1, 1));
+		lblNewLabelDatabase.setText(Messages.DBLoginDialog_4);
+		
+		textDatabase = new Text(grpConnectionType, SWT.BORDER);
+		textDatabase.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));		
+		
+		Label lblUser = new Label(grpConnectionType, SWT.NONE);
+		lblUser.setText(Messages.DBLoginDialog_2);
+		
+		textUser = new Text(grpConnectionType, SWT.BORDER);
+		textUser.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		
+		Label lblPassword = new Label(grpConnectionType, SWT.NONE);
+		lblPassword.setText(Messages.DBLoginDialog_3);
+		
+		textPassword = new Text(grpConnectionType, SWT.BORDER | SWT.PASSWORD);
+		textPassword.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		
+		Label lblLocale = new Label(grpConnectionType, SWT.NONE);
+		lblLocale.setText(Messages.MySQLLoginComposite_lblLocale_text);
+		
+		comboLocale = new Combo(grpConnectionType, SWT.READ_ONLY);
+		comboLocale.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		
+		Label lblSupportSSL = new Label(grpConnectionType, SWT.NONE);
+		lblSupportSSL.setText("Support SSL?");
+		
+		comboSSL = new Combo(grpConnectionType, SWT.READ_ONLY);
+		comboSSL.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		comboSSL.add(PublicTadpoleDefine.YES_NO.YES.toString());
+		comboSSL.add(PublicTadpoleDefine.YES_NO.NO.toString());
+		comboSSL.select(1);
+		
+		othersConnectionInfo = new OthersConnectionRDBGroup(this, SWT.NONE, getSelectDB());
+		othersConnectionInfo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+
+		init();
 	}
 	
 	@Override
@@ -56,6 +176,8 @@ public class PostgresLoginComposite extends MySQLLoginComposite {
 			textDatabase.setText(oldUserDB.getDb());
 			textUser.setText(oldUserDB.getUsers());
 			textPassword.setText(oldUserDB.getPasswd());
+			
+			comboSSL.setText(oldUserDB.getExt1());
 			
 		} else if(ApplicationArgumentUtils.isTestMode()) {
 
@@ -93,12 +215,16 @@ public class PostgresLoginComposite extends MySQLLoginComposite {
 	public boolean makeUserDBDao() {
 		if(!isValidateInput()) return false;
 		
-		final String dbUrl = String.format(
+		String dbUrl = String.format(
 								getSelectDB().getDB_URL_INFO(), 
 								StringUtils.trimToEmpty(textHost.getText()), 
 								StringUtils.trimToEmpty(textPort.getText()), 
 								StringUtils.trimToEmpty(textDatabase.getText())
 							);
+		
+		if(PublicTadpoleDefine.YES_NO.YES.toString().equals(comboSSL.getText())) {
+			dbUrl += "?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+		}
 
 		userDB = new UserDBDAO();
 		userDB.setDbms_types(getSelectDB().getDBToString());
@@ -113,6 +239,8 @@ public class PostgresLoginComposite extends MySQLLoginComposite {
 		userDB.setUsers(StringUtils.trimToEmpty(textUser.getText()));
 		userDB.setPasswd(StringUtils.trimToEmpty(textPassword.getText()));
 		userDB.setLocale(StringUtils.trimToEmpty(comboLocale.getText()));
+		
+		userDB.setExt1(comboSSL.getText());
 		
 		// others connection 정보를 입력합니다.
 		OthersConnectionInfoDAO otherConnectionDAO =  othersConnectionInfo.getOthersConnectionInfo();
