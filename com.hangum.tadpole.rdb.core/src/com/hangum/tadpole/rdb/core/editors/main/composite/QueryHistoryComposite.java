485c0673c7f772bb2b90e13975b65640dacea302
Hyun_Jong Cho <hangum@gmail.com>
2014-04-14 00:22:09 +0900

    코드 리팩토링.

--- com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/editors/main/composite/QueryHistoryComposite.java
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/editors/main/composite/QueryHistoryComposite.java
@@ -38,7 +38,6 @@ import com.hangum.tadpold.commons.libs.core.define.PublicTadpoleDefine;
 import com.hangum.tadpole.commons.dialogs.message.TadpoleMessageDialog;
 import com.hangum.tadpole.commons.dialogs.message.dao.SQLHistoryDAO;
 import com.hangum.tadpole.rdb.core.Messages;
-import com.hangum.tadpole.sql.dao.system.UserDBDAO;
 import com.hangum.tadpole.sql.system.TadpoleSystem_ExecutedSQL;
 import com.hangum.tadpole.sql.util.tables.AutoResizeTableLayout;
 import com.hangum.tadpole.sql.util.tables.SQLHistoryCreateColumn;
@@ -191,8 +190,6 @@ public class QueryHistoryComposite extends Composite {
 			}
 		});
 		btnRefresh.setText(Messages.MainEditor_24);
-		
-//		findHistoryData();
 	}
 	
 	/**
