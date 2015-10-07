4542e4e0cbd06dd5f48d009853319fc703f91f9a
hangum <hangum@218.38.137.27>
2015-10-08 03:46:42 +0900

    add mysql keywords

--- com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/engine/define/SQLConstants.java
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/engine/define/SQLConstants.java
@@ -21,7 +21,7 @@ package com.hangum.tadpole.engine.define;
  */
 public class SQLConstants {
 //	public static final String[] ADVANCED_KEYWORDS = { "PACKAGE", "FUNCTION", "TYPE", "TRIGGER", "MATERIALIZED", "IF", "EACH", "RETURN", "WRAPPED" };
-	public static final String[] MYSQL_KEYWORDS = {"OPTION"};
+	public static final String[] MYSQL_KEYWORDS = {"OPTION", "DEFAULT"};
 
 	public static final String[] SQLITE_KEYWORDS = {
 			"AND", "AS", "ASC", "ATTACH", "AUTOINCREMENT", "BEFORE", "BEGIN", "BETWEEN", "BY", "CASCADE", "CASE",
