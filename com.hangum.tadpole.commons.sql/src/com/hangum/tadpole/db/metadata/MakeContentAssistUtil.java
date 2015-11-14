c7545df22f8348745134bd146e966b45a66be0a5
hangum <hangum@218.38.137.27>
2015-11-15 01:50:18 +0900

    엔진 초기화시 content assist 데이터도 미리 가지고 있도록 수정합니다.

--- com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/db/metadata/MakeContentAssistUtil.java
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/db/metadata/MakeContentAssistUtil.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     hangum - initial API and implementation
  ******************************************************************************/
-package com.hangum.tadpole.rdb.core.editors.main.utils;
+package com.hangum.tadpole.db.metadata;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +18,12 @@ import org.apache.log4j.Logger;
 
 import com.hangum.tadpole.engine.define.DBDefine;
 import com.hangum.tadpole.engine.manager.TadpoleSQLManager;
+import com.hangum.tadpole.engine.query.dao.mysql.ProcedureFunctionDAO;
 import com.hangum.tadpole.engine.query.dao.mysql.TableDAO;
 import com.hangum.tadpole.engine.query.dao.system.UserDBDAO;
-import com.hangum.tadpole.tajo.core.connections.TajoConnectionManager;
+import com.hangum.tadpole.engine.query.sql.DBSystemSchema;
+import com.hangum.tadpole.engine.security.DBAccessCtlManager;
+import com.hangum.tadpole.engine.sql.util.SQLUtil;
 import com.ibatis.sqlmap.client.SqlMapClient;
 
 /**
@@ -29,7 +32,7 @@ import com.ibatis.sqlmap.client.SqlMapClient;
  * @author hangum
  *
  */
-public class MakeContentAssistUtil extends com.hangum.tadpole.db.metadata.MakeContentAssistUtil {
+public class MakeContentAssistUtil {
 	private static final Logger logger = Logger.getLogger(MakeContentAssistUtil.class);
 	
 	/**
@@ -50,6 +53,31 @@ public class MakeContentAssistUtil extends com.hangum.tadpole.db.metadata.MakeCo
        return strConstList;
 	}
 	
+
+//	/**
+//	 * List of assist table column name
+//	 * 
+//	 * @param tableName
+//	 * @return
+//	 */
+//	public String getAssistColumnList(String tableName) {
+//		String strColumnlist = ""; //$NON-NLS-1$
+//		
+//		try {
+//			TableDAO table = mapTableList.get(tableName);
+//			
+//			List<TableColumnDAO> showTableColumns = TadpoleObjectQuery.makeShowTableColumns(userDB, table);
+//			for (TableColumnDAO tableDao : showTableColumns) {
+//				strColumnlist += tableDao.getSysName() + "|"; //$NON-NLS-1$
+//			}
+//			strColumnlist = StringUtils.removeEnd(strColumnlist, "|"); //$NON-NLS-1$
+//		} catch(Exception e) {
+//			logger.error("MainEditor get the table column list", e); //$NON-NLS-1$
+//		}
+//		
+//		return strColumnlist;
+//	}
+//	
 	/**
 	 * List of assist table name 
 	 * 
@@ -74,6 +102,45 @@ public class MakeContentAssistUtil extends com.hangum.tadpole.db.metadata.MakeCo
 		
 		return userDB.getTableListSeparator();
 	}
+	/**
+	 * getView list
+	 * @return
+	 */
+	public String getAssistViewList(final UserDBDAO userDB) {
+		StringBuffer strTablelist = new StringBuffer();
+		try {
+			List<TableDAO> showViews = DBSystemSchema.getViewList(userDB);
+			
+			for (TableDAO tableDao : showViews) {
+				strTablelist.append(tableDao.getSysName()).append("|"); //$NON-NLS-1$
+			}
+		} catch(Exception e) {
+			logger.error("MainEditor get the table list", e); //$NON-NLS-1$
+		}
+		userDB.setViewListSeparator( StringUtils.removeEnd(strTablelist.toString(), "|")); //$NON-NLS-1$
+
+		return userDB.getViewListSeparator();
+	}
+	
+	/**
+	 * getFunctionList
+	 * @return
+	 */
+	public String getFunctionList(final UserDBDAO userDB) {
+		StringBuffer strFunctionlist = new StringBuffer();
+		
+		try {
+			for (ProcedureFunctionDAO tableDao : DBSystemSchema.getFunctionList(userDB)) {
+				strFunctionlist.append(tableDao.getName()).append("|"); //$NON-NLS-1$
+			}
+			
+			userDB.setFunctionLisstSeparator(StringUtils.removeEnd(strFunctionlist.toString(), "|"));
+		} catch (Exception e) {
+			logger.error("showFunction refresh", e); //$NON-NLS-1$
+		}
+		
+		return userDB.getFunctionLisstSeparator(); //$NON-NLS-1$
+	}
 	
 	/**
 	 * 보여 주어야할 테이블 목록을 정의합니다.
@@ -86,7 +153,7 @@ public class MakeContentAssistUtil extends com.hangum.tadpole.db.metadata.MakeCo
 		List<TableDAO> showTables = null;
 				
 		if(userDB.getDBDefine() != DBDefine.TAJO_DEFAULT) {
-			showTables = new TajoConnectionManager().tableList(userDB);			
+			showTables = new ArrayList<TableDAO>();//().tableList(userDB);			
 		} else {
 			SqlMapClient sqlClient = TadpoleSQLManager.getInstance(userDB);
 			showTables = sqlClient.queryForList("tableListOnlyName", userDB.getDb()); //$NON-NLS-1$			
@@ -95,5 +162,22 @@ public class MakeContentAssistUtil extends com.hangum.tadpole.db.metadata.MakeCo
 		/** filter 정보가 있으면 처리합니다. */
 		return getTableAfterwork(showTables, userDB);
 	}
-	
+	/**
+	 * Table 정보 처리 후에 
+	 * 
+	 * @param showTables
+	 * @param userDB
+	 * @return
+	 */
+	protected List<TableDAO> getTableAfterwork(List<TableDAO> showTables, final UserDBDAO userDB) {
+		/** filter 정보가 있으면 처리합니다. */
+		showTables = DBAccessCtlManager.getInstance().getTableFilter(showTables, userDB);
+		
+		// 시스템에서 사용하는 용도록 수정합니다. '나 "를 붙이도록.
+		for(TableDAO td : showTables) {
+			td.setSysName(SQLUtil.makeIdentifierName(userDB, td.getName()));
+		}
+		
+		return showTables;
+	}
 }
