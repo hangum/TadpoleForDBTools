0dc30f0bcb583f6c7735ab0bba7593f5f582d205
hangum <hangum@218.38.137.27>
2015-12-03 20:34:51 +0900

    Update system version numbers

--- com.hangum.tadpole.commons.libs/src/com/hangum/tadpole/commons/libs/core/define/SystemDefine.java
+++ com.hangum.tadpole.commons.libs/src/com/hangum/tadpole/commons/libs/core/define/SystemDefine.java
@@ -21,9 +21,9 @@ import org.eclipse.core.runtime.Platform;
 public class SystemDefine {
 	
 	public static final String NAME = "Tadpole DB Hub";
-	public static final String MAJOR_VERSION = "1.6.6";
-	public static final String SUB_VERSION = "Build(R6)";
-	public static final String RELEASE_DATE = "2015.11.29";
+	public static final String MAJOR_VERSION = "1.6.7";
+	public static final String SUB_VERSION = "Build(R0)";
+	public static final String RELEASE_DATE = "2015.12.03";
 	public static final String INFORMATION = "http://hangum.github.io/TadpoleForDBTools/";
 	
 	public static final String ADMIN_EMAIL = "adi.tadpole@gmail.com";
