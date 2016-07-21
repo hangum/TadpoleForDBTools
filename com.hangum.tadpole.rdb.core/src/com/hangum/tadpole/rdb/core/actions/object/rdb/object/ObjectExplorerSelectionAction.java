14eb033754b2e29d40514fbb1d19df7d6b945471
hangum <hangum@gmail.com>
2016-07-21 17:01:04 +0900

    TableComposite의 오브젝트 선택시 에디터로 보내기 기능 추

---
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/actions/object/rdb/object/ObjectExplorerSelectionAction.java
@@ -0,0 +1,59 @@
+/*******************************************************************************
+ * Copyright (c) 2016 hangum.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     hangum - initial API and implementation
+ ******************************************************************************/
+package com.hangum.tadpole.rdb.core.actions.object.rdb.object;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IWorkbenchWindow;
+
+import com.hangum.tadpole.commons.libs.core.define.PublicTadpoleDefine;
+import com.hangum.tadpole.commons.libs.core.define.PublicTadpoleDefine.OBJECT_TYPE;
+import com.hangum.tadpole.engine.query.dao.mysql.StructObjectDAO;
+import com.hangum.tadpole.engine.query.dao.system.UserDBDAO;
+import com.hangum.tadpole.rdb.core.Messages;
+import com.hangum.tadpole.rdb.core.actions.object.AbstractObjectSelectAction;
+import com.hangum.tadpole.rdb.core.util.FindEditorAndWriteQueryUtil;
+
+/**
+ * Object explorer의 선택 된것을에디터에 붙여 넣기. 
+ * 
+ * @author hangum
+ *
+ */
+public class ObjectExplorerSelectionAction extends AbstractObjectSelectAction {
+	private static final Logger logger = Logger.getLogger(ObjectExplorerSelectionAction.class);
+	public final static String ID = "com.hangum.db.browser.rap.core.actions.object.selection"; //$NON-NLS-1$
+
+	public ObjectExplorerSelectionAction(IWorkbenchWindow window, PublicTadpoleDefine.OBJECT_TYPE actionType) {
+		super(window, actionType);
+		setId(ID + actionType.toString());
+		setText(Messages.get().TableColumnSelectionAction_1);
+	}
+
+	@Override
+	public void run(IStructuredSelection selection, UserDBDAO userDB, OBJECT_TYPE actionType) {
+		if(selection.isEmpty()) return;
+		
+		String strColumnName = "";
+		Object[] arryObj = selection.toArray();
+		for(int i=0; i<arryObj.length; i++) {
+			Object obj = arryObj[arryObj.length-i-1];
+			
+			StructObjectDAO tcDAO = (StructObjectDAO)obj;
+			strColumnName += tcDAO.getSysName() + ", "; //$NON-NLS-1$
+		}
+		strColumnName = StringUtils.removeEnd(strColumnName, ", "); //$NON-NLS-1$
+		
+		FindEditorAndWriteQueryUtil.runAtPosition(strColumnName);
+	}// end method
+	
+}
