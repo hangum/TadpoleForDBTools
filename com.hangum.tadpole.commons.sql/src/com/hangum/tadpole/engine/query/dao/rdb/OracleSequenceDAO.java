b924c6de62f58eda702af240160dc7fe19b372af
jeongjaehong <jeongjaehong@gmail.com>
2016-07-28 20:00:45 +0900

    태그 수정.

--- com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/engine/query/dao/rdb/OracleSequenceDAO.java
+++ com.hangum.tadpole.commons.sql/src/com/hangum/tadpole/engine/query/dao/rdb/OracleSequenceDAO.java
@@ -1,13 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2013 hangum.
+ * Copyright (c) 2016 nilriri.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Lesser Public License v2.1
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * 
  * Contributors:
- *     hangum - initial API and implementation
- *     nilriri - RDBMS information for columns.
+ *     nilriri - Oracle Sequence information.
  ******************************************************************************/
 package com.hangum.tadpole.engine.query.dao.rdb;
 
