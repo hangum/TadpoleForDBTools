31ec5d5f3c1c4fbf0850887d66652ac745857a50
hangum <hangum@gmail.com>
2016-08-02 23:24:43 +0900

    - Add action of table space manager - image optimation

---
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/actions/oracle/TableSpaceManagerAction.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2016 hangum.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     hangum - initial API and implementation
+ ******************************************************************************/
+package com.hangum.tadpole.rdb.core.actions.oracle;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IViewActionDelegate;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.PlatformUI;
+
+import com.hangum.tadpole.engine.query.dao.system.UserDBDAO;
+import com.hangum.tadpole.rdb.core.Messages;
+import com.hangum.tadpole.rdb.core.actions.connections.ext.DownloadSQLiteDBAction;
+import com.hangum.tadpole.rdb.core.viewers.connections.ManagerViewer;
+
+/**
+ * Oracle table space manager action
+ * @author hangum
+ *
+ */
+public class TableSpaceManagerAction implements IViewActionDelegate {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = Logger.getLogger(DownloadSQLiteDBAction.class);
+	private IStructuredSelection sel;
+	
+
+	public TableSpaceManagerAction() {
+		super();
+	}
+
+	@Override
+	public void run(IAction action) {
+		final UserDBDAO userDB = (UserDBDAO)sel.getFirstElement();
+		
+		MessageDialog.openConfirm(null, "Table space manager", "Table space manager");
+	}
+	
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		sel = (IStructuredSelection)selection;
+	}
+
+	@Override
+	public void init(IViewPart view) {
+	}
+
+}
