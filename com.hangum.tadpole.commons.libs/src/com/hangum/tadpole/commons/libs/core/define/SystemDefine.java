e714d903d8367b9a3f6b2ad57184024b8e6faf26
hangum <hangum@gmail.com>
2017-01-11 20:56:03 +0900

    Update information

--- com.hangum.tadpole.commons.libs/src/com/hangum/tadpole/commons/libs/core/define/SystemDefine.java
+++ com.hangum.tadpole.commons.libs/src/com/hangum/tadpole/commons/libs/core/define/SystemDefine.java
@@ -22,8 +22,8 @@ public class SystemDefine {
 	
 	public static final String NAME = "Tadpole DB Hub";
 	public static final String MAJOR_VERSION = "1.7.3";
-	public static final String SUB_VERSION = "Build (r8-3)";
-	public static final String RELEASE_DATE = "2017.01.10";
+	public static final String SUB_VERSION = "Build (r8.6)";
+	public static final String RELEASE_DATE = "2017.01.11";
 	public static final String INFORMATION = "http://hangum.github.io/TadpoleForDBTools/";
 	
 	public static final String ADMIN_EMAIL = "hangum@tadpolehub.com";
