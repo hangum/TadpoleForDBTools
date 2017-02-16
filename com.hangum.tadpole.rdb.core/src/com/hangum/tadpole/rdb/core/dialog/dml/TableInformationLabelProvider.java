9511dfed60a9072288cf1edd3869aac108ccdef3
hangum <hangum@gmail.com>
2017-02-17 08:57:23 +0900

    테이블 정보 팝업의 컬럼 타입의 이미콘이 올바로 보이지 않는 오류 수정.

--- com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/dialog/dml/TableInformationLabelProvider.java
+++ com.hangum.tadpole.rdb.core/src/com/hangum/tadpole/rdb/core/dialog/dml/TableInformationLabelProvider.java
@@ -10,13 +10,12 @@
  ******************************************************************************/
 package com.hangum.tadpole.rdb.core.dialog.dml;
 
-import org.apache.commons.lang.StringUtils;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
-import com.hangum.tadpole.rdb.core.Activator;
-import com.swtdesigner.ResourceManager;
+import com.hangum.tadpole.commons.libs.core.define.PublicTadpoleDefine;
+import com.hangum.tadpole.rdb.core.viewers.object.ObjectExploreDefine;
 
 /**
  * generate statement dml
@@ -30,15 +29,14 @@ public class TableInformationLabelProvider extends LabelProvider implements ITab
 	public Image getColumnImage(Object element, int columnIndex) {
 		if (columnIndex == 0) {
 			ExtendTableColumnDAO columnDao = (ExtendTableColumnDAO) element;
-			if ( StringUtils.equalsIgnoreCase("PK", columnDao.getKey()) ) {
-				return ResourceManager.getPluginImage(Activator.PLUGIN_ID, "resources/icons/objectExplorer/primary_key_column.png");
-			}else if ( StringUtils.equalsIgnoreCase("FK", columnDao.getKey()) ) {
-				return ResourceManager.getPluginImage(Activator.PLUGIN_ID, "resources/icons/objectExplorer/foreign_key_column.png");
-			}else if ( StringUtils.equalsIgnoreCase("MUL", columnDao.getKey()) ) {
-				return ResourceManager.getPluginImage(Activator.PLUGIN_ID, "resources/icons/objectExplorer/multi_key_column.png");
-			}else{
-				return ResourceManager.getPluginImage(Activator.PLUGIN_ID, "resources/icons/objectExplorer/column.png");
-			}
+			
+			Image imageBase = null;
+			if(PublicTadpoleDefine.isPK(columnDao.getKey())) 		imageBase = ObjectExploreDefine.IMAGE_PRIMARY_KEY; 
+			else if(PublicTadpoleDefine.isFK(columnDao.getKey())) 	imageBase = ObjectExploreDefine.IMAGE_FOREIGN_KEY; 
+			else if(PublicTadpoleDefine.isMUL(columnDao.getKey())) imageBase = ObjectExploreDefine.IMAGE_MULTI_KEY;
+			else 											imageBase = ObjectExploreDefine.IMAGE_COLUMN;
+			
+			return imageBase;
 		}
 		return null;
 	}
