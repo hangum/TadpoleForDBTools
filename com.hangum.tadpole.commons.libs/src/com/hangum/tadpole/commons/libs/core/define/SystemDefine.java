58aaf731df8ba40b2d0b701f351c4e330d8350e9
hangum <hangum@gmail.com>
2017-08-10 03:52:59 +0900

    - 컬럼 목에서 설명 위치를 3번째에서 제일 뒤로 옮김. - body text HUB -> Hub 로 수정.

--- com.hangum.tadpole.commons.libs/src/com/hangum/tadpole/commons/libs/core/define/SystemDefine.java
+++ com.hangum.tadpole.commons.libs/src/com/hangum/tadpole/commons/libs/core/define/SystemDefine.java
@@ -22,8 +22,8 @@ public class SystemDefine {
 	
 	public static final String NAME = "Tadpole DB Hub";
 	public static final String MAJOR_VERSION = "1.7.4";
-	public static final String SUB_VERSION = "Build (r13)";
-	public static final String RELEASE_DATE = "2017.08.05";
+	public static final String SUB_VERSION = "Build (r15)";
+	public static final String RELEASE_DATE = "2017.08.10";
 	public static final String INFORMATION = "http://hangum.github.io/TadpoleForDBTools/";
 	
 	public static final String ADMIN_EMAIL = "hangum@tadpolehub.com";
