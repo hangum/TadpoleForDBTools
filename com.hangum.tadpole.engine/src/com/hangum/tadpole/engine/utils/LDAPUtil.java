057f4a36176efe7e42c03cf5375792dd0c2d4d60
hangum <hangum@gmail.com>
2018-05-28 09:13:48 +0900

    initialize project

--- com.hangum.tadpole.engine/src/com/hangum/tadpole/engine/utils/LDAPUtil.java
+++ com.hangum.tadpole.engine/src/com/hangum/tadpole/engine/utils/LDAPUtil.java
@@ -12,10 +12,7 @@ package com.hangum.tadpole.engine.utils;
 
 import org.apache.log4j.Logger;
 
-import com.hangum.tadpole.commons.Messages;
 import com.hangum.tadpole.commons.exception.TadpoleAuthorityException;
-import com.hangum.tadpole.preference.define.GetAdminPreference;
-import com.tadpolehub.db.ldap.core.utils.InternalLDAPUtils;
 
 /**
  * LDAP utils
@@ -27,9 +24,6 @@ public class LDAPUtil {
 	private static final Logger logger = Logger.getLogger(LDAPUtil.class);
 	
 	public static LDAPUtil instance = null;
-//	private static String _ldapHost;		 // LDAP 호스트
-//	private static String _principal;	 // LDAP 쿼리
-	
 	private LDAPUtil() {}
 	
 	public static LDAPUtil getInstance() {
@@ -52,48 +46,6 @@ public class LDAPUtil {
 	 * @return
 	 */
 	public static void isValidation(String userId, String userPwd) throws TadpoleAuthorityException {
-		String url 		= GetAdminPreference.getLDAPURL();
-		String bindDn	= GetAdminPreference.getLDAPBindDN(); 
-		String bindPwd	= GetAdminPreference.getLDAPBindDNPwd();
-		String userDn	= GetAdminPreference.getLDAPUser();
-		String baseDN	= GetAdminPreference.getLDAPBaseDN();
-		
-		try {
-			if(bindDn.equals("")) {
-				InternalLDAPUtils.isLogin(url, baseDN, userId, userPwd);				
-			} else {
-				InternalLDAPUtils.isBindLogin(url, bindDn, bindPwd, baseDN, String.format(userDn, userId), userPwd);
-			}
-		} catch(Exception e) {
-			logger.error("LDAP Login fail" + e.getMessage());
-			throw new TadpoleAuthorityException(Messages.get().PleaseCheckIDpassword + "\n" + e.getMessage());
-		}
 	}
 
-//	/**
-//	 * LDAP Login
-//	 * 
-//	 * @param strEmail
-//	 * @param strPass
-//	 * @deprecated
-//	 */
-//	public static void ldapLogin(String strEmail, String strPass) throws TadpoleAuthorityException {
-//		Hashtable<String, String> properties = new Hashtable<String, String>();
-//		properties.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-//		properties.put(Context.PROVIDER_URL, _ldapHost);
-//		properties.put(Context.SECURITY_AUTHENTICATION, "simple");
-//		properties.put(Context.SECURITY_PRINCIPAL, String.format(_principal, strEmail));
-//		properties.put(Context.SECURITY_CREDENTIALS, strPass);
-//		properties.put("com.sun.jndi.ldap.connect.timeout", "5000");
-//		
-//		DirContext con = null;
-//		try {
-//			con = new InitialDirContext(properties);
-//		} catch (Exception e) {
-//			logger.error("LDAP Login fail" + e.getMessage());
-//			throw new TadpoleAuthorityException(Messages.get().PleaseCheckIDpassword);
-//		} finally {
-//			 if(con != null) try { con.close(); } catch(Exception e) {}
-//		}
-//	}
 }
